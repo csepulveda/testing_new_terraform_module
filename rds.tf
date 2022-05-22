43cdf3777bdbefa608975c7e2efac8374259c990
César Sepúlveda <csepulveda@Cesars-Mac-Studio.local>
2022-05-21 21:39:41 -0400

    init rds database

---
+++ rds.tf
@@ -0,0 +1,84 @@
+locals {
+  name = "walrus-database"
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = local.name
+  description = "Complete PostgreSQL example security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 5432
+      to_port     = 5432
+      protocol    = "tcp"
+      description = "PostgreSQL access from within VPC"
+      cidr_blocks = join(",", concat(module.vpc.database_subnets_cidr_blocks, module.vpc.private_subnets_cidr_blocks))
+    },
+  ]
+}
+
+################################################################################
+# RDS Module
+################################################################################
+
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = local.name
+
+  engine               = "postgres"
+  engine_version       = "14.1"
+  family               = "postgres14"
+  major_engine_version = "14"
+  instance_class       = "db.t4g.small"
+
+  allocated_storage     = 20
+  max_allocated_storage = 100
+
+  db_name  = "walrus-postgres"
+  username = "admin_postgresql"
+  port     = 5432
+
+  multi_az               = false
+  db_subnet_group_name   = module.vpc.database_subnet_group
+  vpc_security_group_ids = [module.security_group.security_group_id]
+
+  maintenance_window              = "Mon:00:00-Mon:03:00"
+  backup_window                   = "03:00-06:00"
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  create_cloudwatch_log_group     = true
+
+  backup_retention_period = 0
+  skip_final_snapshot     = true
+  deletion_protection     = false
+
+  performance_insights_enabled          = true
+  performance_insights_retention_period = 7
+  create_monitoring_role                = true
+  monitoring_interval                   = 60
+  monitoring_role_name                  = "rds-monitoring-role"
+  monitoring_role_description           = "Monitoring role"
+
+  parameters = [
+    {
+      name  = "autovacuum"
+      value = 1
+    },
+    {
+      name  = "client_encoding"
+      value = "utf8"
+    }
+  ]
+
+  db_option_group_tags = {
+    "Sensitive" = "low"
+  }
+  db_parameter_group_tags = {
+    "Sensitive" = "low"
+  }
+}
