353d3994291a201e5abaa8a0d1a3d2130b03106c
César Sepúlveda <csepulveda@Cesars-Mac-Studio.local>
2022-05-21 20:55:15 -0400

    setup s3 backend statefiles

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "dev-terraform-79characters"
+    key = "terraform-statefile"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
